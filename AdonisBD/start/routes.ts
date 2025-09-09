e30fef0c807416936fe94d3433b5d42d2c262bb7
Heloisa Leal <heloisa.dleal@gmail.com>
2025-09-09 16:50:22 -0300

    entregue

--- AdonisBD/start/routes.ts
+++ AdonisBD/start/routes.ts
@@ -14,3 +14,15 @@ router.get('/', async () => {
     hello: 'world',
   }
 })
+
+router.resource('cursos', '#controllers/cursos_controller')
+router.resource('disciplinas', '#controllers/disciplinas_controller')
+router.resource('alunos', '#controllers/alunos_controller')
+
+// Matrículas
+router.get('matriculas', '#controllers/matriculas_controller.index')
+router.post('matriculas', '#controllers/matriculas_controller.store')
+router.delete('matriculas/:alunoId/:disciplinaId',
+  '#controllers/matriculas_controller.destroy')
+
+
